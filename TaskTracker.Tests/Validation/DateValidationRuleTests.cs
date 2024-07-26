11e0ccfb49c0a0ec0152f8c0a17401e02d1d8b73
McDonnell, Robert <rcmcd@hotmail.com>
2024-07-26 14:50:19 -0600

    Added unit tests

---
+++ TaskTracker.Tests/Validation/DateValidationRuleTests.cs
@@ -0,0 +1,28 @@
+﻿using TaskTracker.Validation;
+
+namespace TaskTracker.Tests.Validation
+{
+    public class DateValidationRuleTests
+    {
+        [Fact]
+        public void EmptyStringIsValid()
+        {
+            var sut = new DateValidationRule();
+            Assert.True(sut.Validate(null, null).IsValid);
+        }
+
+        [Fact]
+        public void BadStringIsNotValid()
+        {
+            var sut = new DateValidationRule();
+            Assert.False(sut.Validate("13/40/203", null).IsValid);
+        }
+
+        [Fact]
+        public void GoodDateIsValid()
+        {
+            var sut = new DateValidationRule();
+            Assert.True(sut.Validate(DateTime.Today, null).IsValid);
+        }
+    }
+}
---
+++ TaskTracker.Tests/Validation/RequiredValidationRuleTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Validation;
+
+namespace TaskTracker.Tests.Validation
+{
+    public class RequiredValidationRuleTests
+    {
+        [Fact]
+        public void EmptyStringIsNotValid()
+        {
+            var sut = new RequiredValidationRule();
+            Assert.False(sut.Validate(null, null).IsValid);
+        }
+
+        [Fact]
+        public void GoodStringIsValid()
+        {
+            var sut = new RequiredValidationRule();
+            Assert.True(sut.Validate("TEST", null).IsValid);
+        }
+    }
+}
